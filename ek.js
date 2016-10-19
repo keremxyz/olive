dbdf4e9d68f3657f418ad18c3e9b81e28dfb6469
rumeysaosmanoglu <keremsamet@live.com>
2016-10-19 22:16:39 +0300

    Delete ek.js

--- ek.js
+++
@@ -1,5 +0,0 @@
-if (document.documentElement.clientWidth < 420) {   var idb_thumbs = 345; 
-var idb_title = true;  }
-
-if (document.documentElement.clientWidth > 420) {   var idb_thumbs = 199; 
-var idb_title = true;  }
