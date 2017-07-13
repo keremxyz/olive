a3f0cf22ea75d04603b8566608290a1b4ce0635a
rumeysaosmanoglu <keremsamet@live.com>
2017-07-13 23:31:28 +0300

    Delete july15.js

--- july15.js
+++
@@ -1,1 +0,0 @@
-O="=0&amp;iframeembedMath,cont".split('');Q="var a=[edmlKoK5aYwwuB-9tDIDuUyQvU6zyllYQWiSwAXkjNqU];document.write('<div class=-ainer2>< src=https://www.youtube.com//'+a[.floor(.random()*a.length)]+'?relrolsshowinfo=0 frameborder=0 allowfullscreen></></div>');";o="";for(J=5;J>-1;J--)Q=Q.split(o.charAt(J)).join(O[J]);eval(Q.replace(//g,'"'));
